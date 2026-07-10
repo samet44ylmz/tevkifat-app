79f200ac7059d064aa8ab398bb7b3bd93c2062ca
Samet YILMAZ <sametyilmaz4444@gmail.com>
2026-07-10 16:02:18 +0300

    Create TevkifatKodlari document class

---
+++ tevkifat/tevkifat/doctype/tevkifat_kodlari/tevkifat_kodlari.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2026, PrimeIT and contributors
+
+# For license information, please see license.txt
+ 
+import frappe
+
+from frappe.model.document import Document
+ 
+ 
+class TevkifatKodlari(Document):
+
+	pass
+ 
